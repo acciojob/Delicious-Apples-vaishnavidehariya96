aad87869e97dc009e6f12dbd77b264cff0e3a767
vaishnavidehariya96 <vaishnavidehariya96@gmail.com>
2025-10-28 01:30:07 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+// Message to confirm JS is loaded
+console.log("Apple Portfolio Loaded Successfully! 🍎");
+
+// Select the main image and all thumbnail images
+const mainImage = document.querySelector(".photo-main img");
+const thumbnails = document.querySelectorAll(".photo-album li img");
+
+// When a thumbnail is clicked, change the main image
+thumbnails.forEach(thumbnail => {
+  thumbnail.addEventListener("click", () => {
+    mainImage.src = thumbnail.src;
+  });
+});
