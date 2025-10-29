a02ca5b95c195c4f4399c2a8c7303c2f50560093
vaishnavidehariya96 <vaishnavidehariya96@gmail.com>
2025-10-29 01:47:41 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-// Select the main photo image
-const mainPhoto = document.querySelector(".photo-main img");
+// Wait for the page to fully load
+document.addEventListener("DOMContentLoaded", function() {
 
-// Select all small thumbnail images
-const thumbnails = document.querySelectorAll(".photo-album img");
+  // Select the main image and all the small thumbnail images
+  const mainPhoto = document.querySelector(".photo-main img");
+  const smallPhotos = document.querySelectorAll(".photo-album img");
 
-// Loop through all thumbnails and add a click event to each
-thumbnails.forEach(thumbnail => {
-  thumbnail.addEventListener("click", () => {
-    // Change the main image source to the clicked thumbnail's source
-    mainPhoto.src = thumbnail.src;
+  // Loop through all small images and add a click event
+  smallPhotos.forEach(function(photo) {
+    photo.addEventListener("click", function() {
+      // Change the main photo when a small photo is clicked
+      mainPhoto.src = photo.src;
+    });
   });
-});
 
-// Optional: Log a message to the console (to test if JS is working)
-console.log("Welcome to Accio Jobs - Apple Portfolio Page!");
+  // Just to check if JS is working properly
+  console.log("JS file linked and running!");
+});
